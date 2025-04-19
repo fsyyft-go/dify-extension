fdbeff149d367d967f344bfffd7419c3db07fe5d
FanFusheng <ffs@ppno.net>
2025-04-19 09:09:24 +0800

    feat: 初始化 dify-extension 项目
    - 添加基本的项目结构和配置文件
    - 定义 API 接口和错误码
    - 设置代码生成和构建流程
    - 配置 CI/CD 和代码检查工具

---
+++ internal/app/task/wire.go
@@ -0,0 +1,25 @@
+// Copyright 2025 fsyyft-go
+//
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+//go:build wireinject
+// +build wireinject
+
+package task
+
+import (
+	"github.com/google/wire"
+
+	appconf "github.com/fsyyft-go/dify-extension/internal/conf"
+	apptask "github.com/fsyyft-go/dify-extension/internal/task"
+)
+
+func wireTask(cfg *appconf.Config) (apptask.Hello, func(), error) {
+	// wire.Build 函数用于声明依赖关系图，将所有组件连接在一起。
+	// panic 调用会在编译时被 wire 工具替换为实际的依赖注入代码。
+	// make generate 如果无法生成时，可以尝试使用 wire ./internal/app/task 生成，可以看到更加详细的错误处理。
+	panic(wire.Build(
+		ProviderSet,
+		apptask.ProviderSet,
+	))
+}
